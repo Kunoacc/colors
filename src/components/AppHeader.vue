6113accd226343086e1e93ebbd6829abcf4903d9
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2023-02-21 18:38:31 +0100

    feat: added dark-mode toggle

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,42 @@
 <template>
-  <header class="flex items-center">
-    <app-logo class="dark:text-white text-black"></app-logo>
-    <div class="dark:text-gray-600 text-gray-700 mx-2 leading-none mt-2">
-      built by
-      <a
-        href="https://github.com/kunoacc"
-        class="underline dark:hover:text-white hover:text-black"
-        >BLVCKCODER</a
-      >
+  <header class="items-center flex justify-between">
+    <div class="flex items-center">
+      <app-logo class="dark:text-white text-black"></app-logo>
+      <div class="dark:text-gray-600 text-gray-700 mx-2 leading-none mt-2">
+        built by
+        <a
+          href="https://github.com/kunoacc"
+          class="underline dark:hover:text-white hover:text-black"
+          >BLVCKCODER</a
+        >
+      </div>
     </div>
+
+    <Switch
+      name="theme-toggle"
+      :checked="theme === 'dark'"
+      @change="toggleTheme"
+    >
+      <template v-slot:labelBefore>
+        <sun-icon class="w-5 h-5"></sun-icon>
+      </template>
+      <template v-slot:labelAfter>
+        <moon-icon class="w-4 h-4"></moon-icon>
+      </template>
+    </Switch>
   </header>
 </template>
 
 <script lang="ts" setup>
+import { themeInjectionKey } from "@/providers/injectionKeys";
+import { inject } from "vue";
+import { MoonIcon, SunIcon } from "@heroicons/vue/20/solid";
 import AppLogo from "./AppLogo.vue";
+import Switch from "./Switch.vue";
+
+const { setTheme, theme } = inject(themeInjectionKey, {});
+
+const toggleTheme = (event: any) => {
+  setTheme(event?.currentTarget?.checked ? "dark" : "light");
+};
 </script>
---
+++ src/components/Switch.vue
@@ -0,0 +1,36 @@
+<template>
+  <label
+    :for="props.name"
+    class="inline-flex items-center select-none cursor-pointer"
+  >
+    <slot name="labelBefore"></slot>
+    <span class="relative w-9 h-5 dark:bg-gray-500 bg-gray-400 rounded-xl mx-2">
+      <input type="checkbox" class="hidden" :id="name" v-bind="$attrs" />
+      <span
+        class="absolute w-4 h-4 m-0.5 shadow-gray-600 shadow-lg bg-gray-700 transition-transform ease-in-out duration-200 rounded-lg"
+      ></span>
+    </span>
+    <slot name="labelAfter"></slot>
+  </label>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  name: string;
+}>();
+</script>
+
+<script lang="ts">
+export default {
+  name: "AppSwitch",
+  inheritAttrs: false,
+};
+</script>
+
+<style lang="scss" scoped>
+input:checked {
+  & + span {
+    transform: translateX(100%);
+  }
+}
+</style>
